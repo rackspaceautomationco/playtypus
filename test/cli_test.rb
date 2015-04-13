26ad079ce27d75e2d0605c858ff07f5e2e85b094
Justin Brown <justin.brown@mailtrust.com>
2015-04-13 16:59:47 -0400

    adds test for response logs

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -8,6 +8,12 @@ class CLITest < Playtypus::Test
     @cli.play
   end
 
+  def test_play_with_response_log
+    @cli = Playtypus::CLI.new
+    @cli.stubs(:options).returns({ :call_log => "#{File.dirname(__FILE__)}/", :response_log => "#{File.dirname(__FILE__)}/#{SecureRandom.uuid}"  })
+    @cli.play
+  end
+
   def test_play_with_file
     @cli = Playtypus::CLI.new
     @cli.stubs(:options).returns({ :call_log => "#{File.dirname(__FILE__)}/call_log.json" })
