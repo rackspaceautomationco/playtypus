ada12c8fccaa0773a787817d05aa77ca8df0b0dc
Justin Brown <justin.brown@mailtrust.com>
2015-04-13 16:54:13 -0400

    improves coverage

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class CLITest < Playtypus::Test
-  def setup
+
+  def test_play_with_directory
     @cli = Playtypus::CLI.new
+    @cli.stubs(:options).returns({ :call_log => "#{File.dirname(__FILE__)}/" })
+    @cli.play
+  end
+
+  def test_play_with_file
+    @cli = Playtypus::CLI.new
+    @cli.stubs(:options).returns({ :call_log => "#{File.dirname(__FILE__)}/call_log.json" })
+    @cli.play
   end
 
 end
